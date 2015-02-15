6f7f4bccfad6373b14073cedfca5302e8753632a
b1012205 <dblacktlagoonb@gmail.com>
2015-02-16 00:06:51 +0900

    dd
    dd

--- sound.js
+++ sound.js
@@ -2,8 +2,6 @@
 var playCount = 0;
 
 function sound(){
-    window.setTimeout("play();", 30000);
-    function play(){
         //初回([playCount=0])以外だったら音声ファイルを巻き戻す
         if(playCount > 0){
             document.getElementById("sound-file").currentTime = 0;
@@ -14,6 +12,5 @@ function sound(){
 
         //初回再生が終わった判定用に[playCount]の値を0から1に変更する
         playCount = 1;
-    }
     
 }
