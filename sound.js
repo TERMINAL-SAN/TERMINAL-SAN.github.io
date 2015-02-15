d8fae28e7af055fb5f5efdbafeb6acc948ad49d8
b1012205 <dblacktlagoonb@gmail.com>
2015-02-16 00:18:41 +0900

    ewewe
    ewewew

--- sound.js
+++ sound.js
@@ -2,7 +2,21 @@
 var playCount = 0;
 
 function sound(){
+        //初回([playCount=0])以外だったら音声ファイルを巻き戻す
+        if(playCount > 0){
+            document.getElementById("sound-file").currentTime = 0;
+        }
 
+        //[ID:sound-file]の音声ファイルを再生[play()]する
+        document.getElementById("sound-file").play();
 
-window.location.href = "honhe/";
+        //初回再生が終わった判定用に[playCount]の値を0から1に変更する
+        playCount = 1;
+    
+    function move(){
+        window.location.href = "honhe/";
+        }
+    
+    setTimeout('move()',2500);
+
 }
